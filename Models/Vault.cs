26b20180a00428c25bce1349bfb2edb2b254d495
Jessie <jessiempaxton@gmail.com>
2019-07-19 16:23:34 -0600

    tool working

---
+++ Models/Vault.cs
@@ -0,0 +1,10 @@
+namespace keepr.Models
+{
+  public class Vault
+  {
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private string Description { get; set; }
+
+  }
+}
--- db-setup.sql
+++ db-setup.sql
@@ -1,3 +1,5 @@
+USE kpr;
+
 -- CREATE TABLE users (
 --     id VARCHAR(255) NOT NULL,
 --     username VARCHAR(20) NOT NULL,
