8af8af39c5923977242e3bcdf8fbf9a0e1b636db
Jessie <jessiempaxton@gmail.com>
2019-08-05 14:28:53 -0600

    keep component in progress data not loading

--- wwwsrc/src/Components/Keeps.vue
+++ wwwsrc/src/Components/Keeps.vue
@@ -1,30 +1,22 @@
 <template>
-  <div class="keeps">
-    <!-- WHERE KEEPS RENDER -->
-    <!-- <div class="row keeps mt-2"> -->
-    <div class="col-12">
-      <h2>Welcome Home {{user.username}}</h2>
+  <div class="card keeps" style="width: 16rem;">
+    <img :src="keeps.img" class="card-img-top" alt="image">
+    <div class="card-body">
+      <h5 class="card-title">{{keeps.name}}</h5>
+      <p class="card-text">{{keeps.description}}
+      </p>
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6" v-for="keep in keeps" :key="keep.id">
-      <div class="card" style="width: 16rem;">
-        <img src="" class="card-img-top" alt="image">
-        <div class="card-body">
-          <h5 class="card-title">{{keep.name}}</h5>
-          <p class="card-text">{{keep.description}}
-          </p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" style="background-color: #e2c175">Private: {{keep.isPrivate}} - Views:
-            {{keep.views}}</li>
-          <li class="list-group-item" style="background-color: #e2c175">Shares: {{keep.shares}} - Keeps:
-            {{keep.shares}}</li>
-        </ul>
-        <select v-model="selected" @change="addKeep(keep.id)">
-          <option disabled value>Archive to Collection</option>
-          <option v-for="vault in vaults" :value="vault.id">{{vault.name}}</option>
-        </select>
-      </div>
-    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item" style="background-color: #e2c175">Private: {{keeps.isPrivate}} - Views:
+        {{keeps.views}}</li>
+      <li class="list-group-item" style="background-color: #e2c175">Shares: {{keeps.shares}} - Keeps:
+        {{keeps.shares}}</li>
+    </ul>
+    <select v-model="selected" @change="addKeep(keep.id)">
+      <option disabled value>Archive to Collection</option>
+      <option v-for="vault in vaults" :value="vault.id">{{vault.name}}</option>
+    </select>
+  </div>
   </div>
   </div>
 </template>
@@ -32,28 +24,13 @@
 <script>
   export default {
     name: "keeps",
+    props: ["keepsData"],
     data() {
       return {
-        newVault: {
-          name: "",
-          description: "",
-        },
-        newKeep: {
-          img: "",
-          name: "",
-          description: "",
-          isPrivate: 0,
-        },
         selected: "",
       }
     },
-    mounted() {
-      this.$store.dispatch("getKeeps");
-    },
     computed: {
-      user() {
-        return this.$store.state.user;
-      },
       keeps() {
         return this.$store.state.keeps;
       },
--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,74 +1,39 @@
 <template>
   <div class="home">
-    <!-- WHERE KEEPS RENDER -->
     <div class="row keeps mt-2">
       <div class="col-12">
         <h2>Welcome Home {{user.username}}</h2>
       </div>
-      <div class="col-lg-3 col-md-4 col-sm-6" v-for="keep in keeps" :key="keep.id">
-        <div class="card" style="width: 16rem;">
-          <img :src="keep.img" class="card-img-top" alt="image">
-          <div class="card-body">
-            <h5 class="card-title">{{keep.name}}</h5>
-            <p class="card-text">{{keep.description}}
-            </p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item" style="background-color: #e2c175">Private: {{keep.isPrivate}} - Views:
-              {{keep.views}}</li>
-            <li class="list-group-item" style="background-color: #e2c175">Shares: {{keep.shares}} - Keeps:
-              {{keep.shares}}</li>
-          </ul>
-          <select v-model="selected" @change="addKeep(keep.id)">
-            <option disabled value>Archive to Collection</option>
-            <option v-for="vault in vaults" :value="vault.id">{{vault.name}}</option>
-          </select>
-        </div>
+      <!-- WHERE KEEPS RENDER -->
+      <div class="row">
+        <keeps v-for="keep in keeps" :key="keep.id" :keepsData="keeps" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Keeps from '@/Components/Keeps.vue'
+
   export default {
     name: "home",
-    data() {
-      return {
-        newVault: {
-          name: "",
-          description: "",
-        },
-        newKeep: {
-          img: "",
-          name: "",
-          description: "",
-          isPrivate: 0,
-        },
-        selected: "",
-      }
-    },
-    mounted() {
-      this.$store.dispatch("getKeeps");
-    },
     computed: {
       user() {
         return this.$store.state.user;
       },
       keeps() {
         return this.$store.state.keeps;
-      },
-      vaults() {
-        return this.$store.state.vaults;
       }
     },
-    methods: {
-      addKeep(keepId) {
-        let vaultKeep = { keepId, vaultId: this.selected }
-        this.$store.dispatch("addKeep", vaultKeep)
-      },
-    }
+    mounted() {
+      this.$store.dispatch("getKeeps");
+    },
+    components: {
+      Keeps
+    },
   }
 </script>
+
 <style scoped>
   .keeps {
     background-color: #f4f4f4;
